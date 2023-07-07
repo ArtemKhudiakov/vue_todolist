1530688f20a3d6c937b0756d65009faf4e2b8657
ArtemKhudiakov <artemka86ber@gmail.com>
2023-07-07 07:20:19 -0300

    feat: routers

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import {createApp} from 'vue';
+import {createRouter, createWebHistory} from 'vue-router';
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {path: '/', component: Home},
+        {path: '/tasks', component: Tasks}
+    ]
+});
+
+const app = createApp(App);
+app.use(store);
+app.use(router);
+app.mount('#app');
