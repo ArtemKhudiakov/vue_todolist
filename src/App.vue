1b71c8c964eb52714517332645b5f1580594bd5f
ArtemKhudiakov <artemka86ber@gmail.com>
2023-07-07 07:31:46 -0300

    feat: app, home

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <template>
-
+  <div id="app" class="app">
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-
+export default {
+  name: 'App'
+}
 </script>
 
 <style>
+html, body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
 
+.app {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,39 @@
 <template>
-
+  <div :class="$style.container">
+    <h1 :class="$style.title">Главная страница</h1>
+    <router-link to="/tasks" :class="$style.link">Перейти к списку задач</router-link>
+  </div>
 </template>
 
 <script>
-
+export default {
+  name: 'Home'
+}
 </script>
 
-<style>
+<style module>
 
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+.title {
+  font-size: 32px;
+  margin-bottom: 20px;
+}
+
+.link {
+  color: #2D4356;
+  font-size: 18px;
+  text-decoration: none;
+  transition: color 0.3s ease;
+}
+
+.link:hover {
+  color: #435B66;
+}
 </style>
